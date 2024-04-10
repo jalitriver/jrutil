e858ab456778aed0dddd7e6946dc6d22d808ea3e
Paul Serice <paul@serice.net>
2024-04-10 15:34:00 -0500

    Renamed MapToSlice() to MapToPairs().

--- pair.go
+++ pair.go
@@ -15,7 +15,7 @@ func MakePair[T1 any, T2 any](first T1, second T2) Pair[T1, T2] {
 	}
 }
 
-// MapToSlice converts the key/value pairs in the unordered input map
+// MapToPairs converts the key/value pairs in the unordered input map
 // m to an unordered slice of key/value pairs.  For example, if you
 // want to sort the key/value pairs in a map by value, you can do the
 // following:
@@ -28,7 +28,7 @@ func MakePair[T1 any, T2 any](first T1, second T2) Pair[T1, T2] {
 //     }
 //     
 //     // Convert the map into a list of key/value pairs.
-//     kvPairs := jrutil.MapToSlice(counts)
+//     kvPairs := jrutil.MapToPairs(counts)
 //     
 //     // Sort the key/value pairs.
 //     sort.Slice(kvPairs, func (i, j int) bool {
@@ -39,7 +39,7 @@ func MakePair[T1 any, T2 any](first T1, second T2) Pair[T1, T2] {
 //     for _, kvPair := range kvPairs {
 //     	fmt.Printf("%v: %v\n", kvPair.First, kvPair.Second)
 //     }
-func MapToSlice[T1 comparable, T2 any](m map[T1]T2) []Pair[T1, T2] {
+func MapToPairs[T1 comparable, T2 any](m map[T1]T2) []Pair[T1, T2] {
 	var result []Pair[T1, T2]
 	for k, v := range m {
 		result = append(result, MakePair(k, v))
