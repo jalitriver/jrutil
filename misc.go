0c7b0abe5640b2613829901c383c2b1558d494d5
Paul Serice <paul@serice.net>
2024-04-10 22:30:47 -0500

    Add Merge() and MergeSort() for slices.

--- misc.go
+++ misc.go
@@ -1,6 +1,7 @@
 package jrutil
 
 import (
+	"cmp"
 	crand "crypto/rand"
 	"math/rand/v2"
 )
@@ -38,4 +39,79 @@ func NewRand() (*rand.Rand, error) {
 		return nil, err
 	}
 	return rand.New(rand.NewChaCha8([32]byte(bs))), nil
+}
+
+// Merge merges the two sorted slices (xs and ys) and returns a new
+// sorted slice that contains all of the elements from the two sorted
+// input slices.  This function performs a shallow copy when copying
+// values from xs and ys to the result.  Thus, if complex values are
+// being sorted, xs and ys should hold pointers (for which shallow
+// copying is correct).
+func Merge[T cmp.Ordered](
+	xs []T,
+	ys []T,
+	f func(x, y T) bool) []T {
+
+	// Get the length of both slices.
+	xLen := uint64(len(xs))
+	yLen := uint64(len(ys))
+
+	// Generate the slice to return.
+	rLen := uint64(xLen + yLen)
+	result := make([]T, rLen)
+
+	// Merge the lists into the result.
+	xIndex := uint64(0)
+	yIndex := uint64(0)
+	for i := uint64(0); i < rLen; i++ {
+
+		// When we run out of xs, the next value must come from ys.
+		if xIndex >= xLen {
+			result[i] = ys[yIndex]
+			yIndex++
+			continue
+		}
+
+		// When we run out of ys, the next value must come from xs.
+		if yIndex >= yLen {
+			result[i] = xs[xIndex]
+			xIndex++
+			continue
+		}
+
+		// We still have values in both xs and ys.  Because xs and ys
+		// are both sorted, the value at xIndex is the smallest value
+		// in xs, and the value at yIndex is the smallest value in ys.
+		// We just need to compare the two values at xIndex and yIndex
+		// and copy the smaller to the result.
+		if f(xs[xIndex], ys[yIndex]) {
+			result[i] = xs[xIndex]
+			xIndex++
+		} else {
+			result[i] = ys[yIndex]
+			yIndex++
+		}
+
+	}
+
+	return result
+}
+
+// MergeSort returns a new, sorted slice based on the comparison
+// function f.
+func MergeSort[T cmp.Ordered](
+	xs []T,
+	LessThanFn func(x1, x2 T) bool) []T {
+
+	// Base case.
+	if len(xs) <= 1 {
+		return xs
+	}
+
+	// Divide and conquer.
+	mid := uint64(len(xs) / 2)
+	return Merge(
+		MergeSort(xs[:mid], LessThanFn),
+		MergeSort(xs[mid:], LessThanFn),
+		LessThanFn)
 }
---
+++ misc_test.go
@@ -0,0 +1,134 @@
+package jrutil
+
+import (
+	"cmp"
+	"slices"
+	"testing"
+)
+
+func TestMerge(t *testing.T) {
+	type Data struct {
+		xs       []int
+		ys       []int
+		expected []int
+	}
+
+	data := []Data{
+		{
+			xs:       []int{},
+			ys:       []int{},
+			expected: []int{},
+		},
+		{
+			xs:       []int{0},
+			ys:       []int{},
+			expected: []int{0},
+		},
+		{
+			xs:       []int{},
+			ys:       []int{0},
+			expected: []int{0},
+		},
+		{
+			xs:       []int{0, 1},
+			ys:       []int{},
+			expected: []int{0, 1},
+		},
+		{
+			xs:       []int{},
+			ys:       []int{0, 1},
+			expected: []int{0, 1},
+		},
+		{
+			xs:       []int{0, 1, 2},
+			ys:       []int{},
+			expected: []int{0, 1, 2},
+		},
+		{
+			xs:       []int{},
+			ys:       []int{0, 1, 2},
+			expected: []int{0, 1, 2},
+		},
+		{
+			xs:       []int{0},
+			ys:       []int{1},
+			expected: []int{0, 1},
+		},
+		{
+			xs:       []int{1},
+			ys:       []int{0},
+			expected: []int{0, 1},
+		},
+		{
+			xs:       []int{0, 2},
+			ys:       []int{1},
+			expected: []int{0, 1, 2},
+		},
+		{
+			xs:       []int{1},
+			ys:       []int{0, 2},
+			expected: []int{0, 1, 2},
+		},
+		{
+			xs:       []int{0, 2},
+			ys:       []int{1, 3},
+			expected: []int{0, 1, 2, 3},
+		},
+	}
+
+	for _, d := range data {
+		actual := Merge(d.xs, d.ys, cmp.Less[int])
+		if !slices.Equal(d.expected, actual) {
+			t.Errorf(
+				"Merge(%v, %v): expected=%v  actual=%v",
+				d.xs, d.ys, d.expected, actual)
+		}
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	type Data struct {
+		xs       []int
+		expected []int
+	}
+
+	data := []Data{
+		{
+			xs:       []int{},
+			expected: []int{},
+		},
+		{
+			xs:       []int{0},
+			expected: []int{0},
+		},
+		{
+			xs:       []int{0, 1},
+			expected: []int{0, 1},
+		},
+		{
+			xs:       []int{1, 0},
+			expected: []int{0, 1},
+		},
+		{
+			xs:       []int{0, 1, 2},
+			expected: []int{0, 1, 2},
+		},
+		{
+			xs:       []int{2, 1, 0},
+			expected: []int{0, 1, 2},
+		},
+		{
+			xs:       []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+			expected: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+	}
+
+	for _, d := range data {
+		actual := MergeSort(d.xs, cmp.Less[int])
+		if !slices.Equal(d.expected, actual) {
+			t.Errorf(
+				"MergeSort(%v): expected=%v  actual=%v",
+				d.xs, d.expected, actual)
+		}
+	}
+}
