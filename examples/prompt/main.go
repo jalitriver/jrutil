ffa7855dcb8dd62426662aeeb0067ece3593d32a
Paul Serice <paul@serice.net>
2024-04-26 14:24:40 -0500

    Add Prompt() and PromptUnbuffered().

---
+++ examples/prompt/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/jalitriver/jrutil"
+)
+
+func main() {
+	var err error
+	var name string
+
+	stdin := bufio.NewReader(os.Stdin)
+	stdout := bufio.NewWriter(os.Stdout)
+
+	for {	
+		name, err = jrutil.Prompt(stdin, stdout, "Name: ")
+		///name, err = jrutil.PromptUnbuffered("Name: ")
+		if name != "" || err == nil {
+			name = strings.TrimSpace(name)
+			fmt.Printf("%s\n", name)
+		}
+		if err != nil {
+			goto out
+		}
+	}
+
+out:
+
+	if err != nil && err != io.EOF {
+		fmt.Fprintf(os.Stderr, "*** Error: %v\n", err)
+		os.Exit(1)
+	}
+	
+}
---
+++ prompt.go
@@ -0,0 +1,101 @@
+package jrutil
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strings"
+)
+
+// Prompt prompts on fout for input and then reads a line of text from
+// fin.  The line of text is returned with the trailing EOL sequence
+// removed.
+//
+// In the common case of wanting to read and write from and to
+// os.Stdin and os.Stdout, they should be wrapped to be buffered as
+// follows which should only happen once probably early in your main()
+// function:
+//
+//  fin := bufio.NewReader(os.Stdin)
+//  fout := bufio.NewWriter(os.Stdout)
+//
+// The reason for having to use buffered I/O for stdin and stdout is
+// to avoid conflicts with other parts of the code that might need to
+// use stdin and stdout.  You cannot mix buffered I/O and non-buffered
+// I/O without losing some bytes along the way.  You cannot even
+// create multiple buffered I/O wrappers around stdin or stdout
+// without losing bytes along the way.
+//
+// Also see PromptUnbuffered().
+func Prompt(
+	fin *bufio.Reader,
+	fout *bufio.Writer,
+	prompt string,
+) (string, error) {
+	var err error
+	var line string
+
+	// Write the prompt.
+	_, err = fout.WriteString(prompt)
+	if err != nil {
+		return "", err
+	}
+	err = fout.Flush()
+	if err != nil {
+		return "", err
+	}
+
+	// Read line.
+	line, err = fin.ReadString('\n')
+	if err != nil {
+		return line, err
+	}
+	if len(line) >= 1 {
+		line = line[:len(line)-1]
+	}
+
+	return line, nil
+}
+
+// Prompt prompts on os.Stdout for input and then reads a line of text
+// from os.Stdin.  The line of text is returned with the trailing EOL
+// sequence removed.
+//
+// Also see Prompt().
+func PromptUnbuffered(prompt string) (string, error) {
+	var err error
+	var line strings.Builder
+
+	// Write the prompt.
+	_, err = os.Stdout.WriteString(prompt)
+	if err != nil {
+		return "", err
+	}
+
+	// Read line.
+	ch := make([]byte, 1)
+	for {
+
+		// Get the next character.
+		_, err = os.Stdin.Read(ch)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return line.String(), err
+		}
+
+		// Check for EOL.
+		if ch[0] == '\n' {
+			break
+		}
+
+		// Append this character to the result.
+		err = line.WriteByte(ch[0])
+		if err != nil {
+			return line.String(), err
+		}
+	}
+
+	return line.String(), err
+}
